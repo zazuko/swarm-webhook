067d38498686b70a38fea839dcd7fd29bb66c619
Ludovic Muller <ludovicmuller1@gmail.com>
2021-06-28 20:40:07 +0200

    add some logs

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ const docker = new Docker();
 const WEBHOOK_ENABLED_LABEL = 'swarm.webhook.enabled';
 const WEBHOOK_NAME_LABEL = 'swarm.webhook.name';
 const port = process.env.SERVER_PORT || 3000;
-const refreshInterval = +(process.env.REFRESH_INTERVAL || 2_000);
-const restartDelay = +(process.env.RESTART_DELAY || 8_000);
+const host = process.env.SERVER_HOST || '::';
+const refreshInterval = +(process.env.REFRESH_INTERVAL || 1_000);
 
 let services: Docker.Service[] = [];
 
@@ -20,6 +20,16 @@ let services: Docker.Service[] = [];
  * @returns a promise that resolves after `ms` milliseconds.
  */
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+/**
+ * Log a message.
+ *
+ * @param message Message to log.
+ */
+const logMessage = (message: string) => {
+  const date = new Date();
+  console.log(`[${date}] - ${message}`);
+};
 
 /**
  * Update the list of enabled services.
@@ -76,61 +86,27 @@ const autoUpdateServicesList = async (): Promise<NodeJS.Timeout> => {
   await autoUpdateServicesList();
 
   // list all containers having the `WEBHOOK_ENABLED_LABEL` label set to `true`
-  server.get('/', async (_request, _reply) => services.map((service) => ({
-    name: service.Spec?.Name,
-    labels: service.Spec?.Labels,
-    createdAt: service.CreatedAt,
-    updatedAt: service.UpdatedAt,
-  })));
+  server.get('/', async (_request, _reply) => {
+    logMessage('list enabled services');
+
+    return services.map((service) => ({
+      name: service.Spec?.Name,
+      labels: service.Spec?.Labels,
+      createdAt: service.CreatedAt,
+      updatedAt: service.UpdatedAt,
+    }));
+  });
 
   // start a container
   server.get<{
     Params: Record<string, string>;
   }>('/start/:service', async (request, _reply) => {
-    const targetService = filterServicesByName(`${request.params.service}`);
+    const targetService = `${request.params.service}`;
+    logMessage(`start '${targetService}'`);
+    const targetServices = filterServicesByName(targetService);
 
     const update = await Promise.all(
-      targetService.map((service) => docker.getService(service.ID).update({
-        ...service.Spec,
-        Mode: {
-          ...service.Spec?.Mode,
-          Replicated: {
-            ...service.Spec?.Mode?.Replicated,
-            Replicas: 1,
-          },
-        },
-        version: service.Version?.Index,
-      })),
-    );
-
-    return update;
-  });
-
-  // force the restart of a container
-  server.get<{
-    Params: Record<string, string>;
-  }>('/restart/:service', async (request, _reply) => {
-    let targetService = filterServicesByName(`${request.params.service}`);
-
-    await Promise.all(
-      targetService.map((service) => docker.getService(service.ID).update({
-        ...service.Spec,
-        Mode: {
-          ...service.Spec?.Mode,
-          Replicated: {
-            ...service.Spec?.Mode?.Replicated,
-            Replicas: 0,
-          },
-        },
-        version: service.Version?.Index,
-      })),
-    );
-
-    await delay(restartDelay);
-
-    targetService = filterServicesByName(`${request.params.service}`);
-    const update = await Promise.all(
-      targetService.map((service) => docker.getService(service.ID).update({
+      targetServices.map((service) => docker.getService(service.ID).update({
         ...service.Spec,
         Mode: {
           ...service.Spec?.Mode,
@@ -150,10 +126,12 @@ const autoUpdateServicesList = async (): Promise<NodeJS.Timeout> => {
   server.get<{
     Params: Record<string, string>;
   }>('/stop/:service', async (request, _reply) => {
-    const targetService = filterServicesByName(`${request.params.service}`);
+    const targetService = `${request.params.service}`;
+    logMessage(`stop '${targetService}'`);
+    const targetServices = filterServicesByName(targetService);
 
     const update = await Promise.all(
-      targetService.map((service) => docker.getService(service.ID).update({
+      targetServices.map((service) => docker.getService(service.ID).update({
         ...service.Spec,
         Mode: {
           ...service.Spec?.Mode,
@@ -170,11 +148,11 @@ const autoUpdateServicesList = async (): Promise<NodeJS.Timeout> => {
   });
 
   // webhook server listening of specified port
-  server.listen(port, '::', (err, address) => {
+  server.listen(port, host, (err, address) => {
     if (err) {
       console.error(err);
       process.exit(1);
     }
-    console.log(`Server listening at: ${address}`);
+    logMessage(`Server listening at: ${address}`);
   });
 })();
