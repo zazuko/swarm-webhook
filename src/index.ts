fd687465b0a4e27e7ae5dde293761f58b7b52510
Ludovic Muller <ludovicmuller1@gmail.com>
2021-06-30 11:11:09 +0200

    force update

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,6 @@ const WEBHOOK_ENABLED_LABEL = 'swarm.webhook.enabled';
 const WEBHOOK_NAME_LABEL = 'swarm.webhook.name';
 const port = process.env.SERVER_PORT || 3000;
 const host = process.env.SERVER_HOST || '::';
-const refreshInterval = +(process.env.REFRESH_INTERVAL || 1_000);
-
-let services: Docker.Service[] = [];
 
 /**
  * Log a message.
@@ -24,13 +21,13 @@ const logMessage = (message: string) => {
 };
 
 /**
- * Update the list of enabled services.
+ * Get the list of enabled services.
  * An enabled service is a service having the `WEBHOOK_ENABLED_LABEL` label set to `"true"`.
  */
-const updateEnabledServices = async (): Promise<void> => {
+const getEnabledServices = async (): Promise<Docker.Service[]> => {
   const allServices = await docker.listServices();
 
-  services = allServices.filter((service) => {
+  return allServices.filter((service) => {
     if (!service.Spec?.Labels) {
       return false;
     }
@@ -46,10 +43,14 @@ const updateEnabledServices = async (): Promise<void> => {
 /**
  * Get the list of enabled services having a specific name.
  *
+ * @param services list of Docker services to filer on.
  * @param name value of the `WEBHOOK_NAME_LABEL` label to filter on.
  * @returns a list of services having the label matching the name parameter.
  */
-const filterServicesByName = (name: string): Docker.Service[] => services.filter((service) => {
+const filterServicesByName = (
+  services: Docker.Service[],
+  name: string,
+): Docker.Service[] => services.filter((service) => {
   if (!service.Spec?.Labels) {
     return false;
   }
@@ -61,25 +62,12 @@ const filterServicesByName = (name: string): Docker.Service[] => services.filter
   return false;
 });
 
-/**
- * Enable auto-updating the list of enabled services.
- *
- * @returns the interval identifier.
- */
-const autoUpdateServicesList = async (): Promise<NodeJS.Timeout> => {
-  await updateEnabledServices();
-
-  return setInterval(async () => {
-    await updateEnabledServices();
-  }, refreshInterval);
-};
-
 (async () => {
-  await autoUpdateServicesList();
-
   // list all containers having the `WEBHOOK_ENABLED_LABEL` label set to `true`
   server.get('/', async (_request, _reply) => {
     logMessage('list enabled services');
+
+    const services = await getEnabledServices();
 
     return services.map((service) => ({
       name: service.Spec?.Name,
@@ -95,20 +83,55 @@ const autoUpdateServicesList = async (): Promise<NodeJS.Timeout> => {
   }>('/start/:service', async (request, _reply) => {
     const targetService = `${request.params.service}`;
     logMessage(`start '${targetService}'`);
-    const targetServices = filterServicesByName(targetService);
+    const services = await getEnabledServices();
+    const targetServices = filterServicesByName(services, targetService);
 
     const update = await Promise.all(
-      targetServices.map((service) => docker.getService(service.ID).update({
-        ...service.Spec,
-        Mode: {
-          ...service.Spec?.Mode,
-          Replicated: {
-            ...service.Spec?.Mode?.Replicated,
-            Replicas: 1,
+      targetServices.map(async (service) => {
+        const dockerService = docker.getService(service.ID);
+        let version = service.Version?.Index;
+        const replicas = service.Spec?.Mode?.Replicated?.Replicas || 0;
+
+        if (replicas > 0) {
+          await dockerService.update({
+            ...service.Spec,
+            Mode: {
+              ...service.Spec?.Mode,
+              Replicated: {
+                ...service.Spec?.Mode?.Replicated,
+                Replicas: 0,
+              },
+            },
+            TaskTemplate: {
+              ...service.Spec?.TaskTemplate,
+              ForceUpdate: version,
+            },
+            version,
+          });
+
+          const updatedDockerService = docker.getService(service.ID);
+          const inspected = await updatedDockerService.inspect();
+          if (inspected?.Version?.Index) {
+            version = inspected?.Version?.Index;
+          }
+        }
+
+        return dockerService.update({
+          ...service.Spec,
+          Mode: {
+            ...service.Spec?.Mode,
+            Replicated: {
+              ...service.Spec?.Mode?.Replicated,
+              Replicas: 1,
+            },
           },
-        },
-        version: service.Version?.Index,
-      })),
+          TaskTemplate: {
+            ...service.Spec?.TaskTemplate,
+            ForceUpdate: version,
+          },
+          version,
+        });
+      }),
     );
 
     return update;
@@ -120,7 +143,8 @@ const autoUpdateServicesList = async (): Promise<NodeJS.Timeout> => {
   }>('/stop/:service', async (request, _reply) => {
     const targetService = `${request.params.service}`;
     logMessage(`stop '${targetService}'`);
-    const targetServices = filterServicesByName(targetService);
+    const services = await getEnabledServices();
+    const targetServices = filterServicesByName(services, targetService);
 
     const update = await Promise.all(
       targetServices.map((service) => docker.getService(service.ID).update({
